bc37b080741b1d94416410c4017549225532e604
kyle-maxwell <kylemaxwell7@gmail.com>
2019-01-15 09:22:31 -0800

    Upload and Backend Proccessing

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import os
 from werkzeug import secure_filename
 from flask import Flask, render_template, request
 import pandas as pd
+from ML.KMeans import data_classifier
 
 app = Flask(__name__)
 
@@ -19,8 +20,9 @@ def test():
 def upload_file():
 	file = request.files['file']
 	df = pd.read_csv(file)
-	print(df)
-	return json.dumps("WORKED?")
+
+	resp = data_classifier(df)
+	return json.dumps(resp)
    	
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 3000), debug=True)

ff011fc27cd2c0b1e923f36841261bc75739e93c
kyle-maxwell <kylemaxwell7@gmail.com>
2019-01-22 11:40:51 -0800

    Home Page

--- ML/KMeans.py
+++ ML/KMeans.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import uuid
 import numpy as np
 import pandas as pd
 from similarity.normalized_levenshtein import NormalizedLevenshtein
@@ -22,20 +23,36 @@ def data_classifier(df):
 	uncert = uncert.set_index(arr1)
 
 
-	data = {}
+	# data = {}
+	# for i in range(clusters):
+	#     cluster = {}
+	#     fields = pd.Series(arr1[kmeans.labels_==i])
+	#     for field in fields:
+	#         cluster[field] = uncert.loc[field][i]
+	#     data["Cluster " + str(i)] = cluster
+
+	classifications_obj = {}
+	fields_obj = {}
 	for i in range(clusters):
 	    cluster = {}
 	    fields = pd.Series(arr1[kmeans.labels_==i])
-	    for field in fields:
-	        cluster[field] = uncert.loc[field][i]
-	    data["Cluster " + str(i)] = cluster
+	    for field_name in fields:
+	        field_id = "field-" + str(uuid.uuid4())
+	        cluster[field_id] = uncert.loc[field_name][i]
+	        fields_obj[field_id] = {"field_name" : field_name}
+	    classifications_obj["classification-" + str(uuid.uuid4())] = cluster
 
+	data = {}
+	data["Classifcations"] = classifications_obj
+	data["Fields"] = fields_obj
 	return(json.dumps(data, sort_keys=True, indent=4))
 
 
-def dist(x, y): # computationally effcient equclidean distance
-    return np.sqrt(np.dot(x, x) - 2 * np.dot(x, y) + np.dot(y, y))
+def dist(x, y): # computationally effcient euclidean distance
+    dist = np.sqrt(np.dot(x, x) - 2 * np.dot(x, y) + np.dot(y, y))
+    return np.around(dist, 3)
 
-def mean_dist(DF, X):
+def mean_dist(DF, X): # TODO could use to order the classifications
     dists = DF.apply(dist, args=(X, ), axis=1)
     return dists.mean()
+
